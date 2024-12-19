6ff80c95ecb6acc2fc7a203d3976d600d56c1ca1
Nitesh Negi <nitesh_negi@outlook.com>
2024-12-19 23:01:48 +0530

    first commit

---
+++ components/Main.jsx
@@ -0,0 +1,17 @@
+export default function Main() {
+  return (
+      <main>
+          <h1>Fun facts about Apple Inc.</h1>
+          <ul className="facts-list">
+              <li>Founded in 1976: Apple was founded by Steve Jobs, Steve Wozniak, and Ronald Wayne on April 1, 1976, in Cupertino, California.</li>
+              <li>Started in a Garage: The company famously started in the garage of Steve Jobs' family home.</li>
+              <li>First Product: The Apple I, hand-built by Wozniak, was Apple's first computer, sold for $666.66.</li>
+              <li>Iconic Logo: The first logo depicted Isaac Newton under an apple tree. It was later replaced by the rainbow-striped Apple logo in 1977, designed by Rob Janoff.</li>
+          </ul>
+      </main>
+  )
+}
+
+
+
+
---
+++ components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+      <header>
+          <nav>
+              <img src="./Apple.png" alt="React logo" />
+              <span>AppleFacts</span>
+          </nav>
+      </header>
+  )
+}
